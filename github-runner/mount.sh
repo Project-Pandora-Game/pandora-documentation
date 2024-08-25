1f4e40ae4c7d9057e1d2e49153ce3e0a6c91a727
jomshir98 <jomshir98@protonmail.com>
2024-08-25 21:00:42 +0000

    [REMOVE] Drop self-hosted GitHub runner

--- github-runner/mount.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ueo pipefail
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-TMPFS_SIZE=${TMPFS_SIZE:-"4G"}
-
-mount -t tmpfs -o size=4G tmpfs ${SCRIPT_DIR}/cache
-chmod -R a+rwX ${SCRIPT_DIR}/cache
